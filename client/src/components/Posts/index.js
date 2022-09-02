905480286c764eb13a00a69a13e121305e06a045
Jp Sia <johnpaulsia4@gmail.com>
2022-09-02 21:28:36 +0800

    Rename files

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -1,7 +1,7 @@
 import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Post from './Post/Post';
+import Post from './Post';
 import useStyles from './styles';
 
 const Posts = ({ setCurrentId }) => {
